d5efc64fb6d588507f39b15e1189e1255c6bbedb
Siddharth Kumar <76401464+siddharthkum@users.noreply.github.com>
2024-02-26 23:01:57 +0530

    Add files via upload

---
+++ fall-down/PlayFallDown.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+@SuppressWarnings("serial")
+public class PlayFallDown extends JFrame
+{
+	public PlayFallDown()
+	{
+		getContentPane().setLayout(new FlowLayout());
+		add(new FallDownComponent());
+		pack();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public static void main(String[] args)
+	{
+		new PlayFallDown();
+	}
+}
